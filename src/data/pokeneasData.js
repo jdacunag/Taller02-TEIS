b2f1c067b5cb0625512e1765124c4c7ee81d8f5e
jdacunag <114156494+jdacunag@users.noreply.github.com>
2024-11-10 21:56:39 -0500

    Update pokeneasData.js

--- src/data/pokeneasData.js
+++ src/data/pokeneasData.js
@@ -4,7 +4,7 @@ const pokeneas = [
       nombre: 'Pokenea1',
       altura: 1.5,
       habilidad: 'Invisibilidad',
-      imagen: 'https://storage.googleapis.com/tu-bucket/pokenea1.jpg',
+      imagen: 'https://storage.googleapis.com/pokeneas-taller02/(%E2%88%A9%5Eo%5E)%E2%8A%83%E2%94%81%E2%98%86.jpg',
       fraseFilosofica: 'El conocimiento es poder.'
     },
     {
@@ -12,7 +12,7 @@ const pokeneas = [
       nombre: 'Pokenea2',
       altura: 1.2,
       habilidad: 'Velocidad',
-      imagen: 'https://storage.googleapis.com/tu-bucket/pokenea2.jpg',
+      imagen: 'https://storage.googleapis.com/pokeneas-taller02/288149-origen-todos-nombres-pokemon-i.webp',
       fraseFilosofica: 'La paciencia es una virtud.'
     },
     {
@@ -20,7 +20,7 @@ const pokeneas = [
       nombre: 'Pokenea3',
       altura: 1.8,
       habilidad: 'Fuerza',
-      imagen: 'https://storage.googleapis.com/tu-bucket/pokenea3.jpg',
+      imagen: 'https://storage.googleapis.com/pokeneas-taller02/004.png',
       fraseFilosofica: 'La fuerza reside en el corazón.'
     },
     {
@@ -28,7 +28,7 @@ const pokeneas = [
       nombre: 'Pokenea4',
       altura: 1.3,
       habilidad: 'Regeneración',
-      imagen: 'https://storage.googleapis.com/tu-bucket/pokenea4.jpg',
+      imagen: 'https://storage.googleapis.com/pokeneas-taller02/Cubone.jpg',
       fraseFilosofica: 'El cambio es la única constante.'
     },
     {
@@ -36,7 +36,7 @@ const pokeneas = [
       nombre: 'Pokenea5',
       altura: 1.6,
       habilidad: 'Telepatía',
-      imagen: 'https://storage.googleapis.com/tu-bucket/pokenea5.jpg',
+      imagen: 'https://storage.googleapis.com/pokeneas-taller02/Estos%20son%20los%20100%20Pok%C3%A9mon%20m%C3%A1s%20cuquis%20y%20su%20ataque%20especial%20se%20llama_%20mu%C3%A9rete%20de%20amor.jpg',
       fraseFilosofica: 'Todo comienza en la mente.'
     },
     {
@@ -44,7 +44,7 @@ const pokeneas = [
       nombre: 'Pokenea6',
       altura: 1.7,
       habilidad: 'Control del tiempo',
-      imagen: 'https://storage.googleapis.com/tu-bucket/pokenea6.jpg',
+      imagen: 'https://storage.googleapis.com/pokeneas-taller02/Pokem%C3%B3n%20PNG%20descarga%20gratis.jpg',
       fraseFilosofica: 'El tiempo es relativo.'
     },
     {
@@ -52,7 +52,7 @@ const pokeneas = [
       nombre: 'Pokenea7',
       altura: 1.4,
       habilidad: 'Resistencia extrema',
-      imagen: 'https://storage.googleapis.com/tu-bucket/pokenea7.jpg',
+      imagen: 'https://storage.googleapis.com/pokeneas-taller02/VULPIX.jpg',
       fraseFilosofica: 'La persistencia rompe todas las barreras.'
     },
     {
@@ -60,7 +60,7 @@ const pokeneas = [
       nombre: 'Pokenea8',
       altura: 1.9,
       habilidad: 'Reflejos',
-      imagen: 'https://storage.googleapis.com/tu-bucket/pokenea8.jpg',
+      imagen: 'https://storage.googleapis.com/pokeneas-taller02/c157545ab985be295c9566b427102231.jpg',
       fraseFilosofica: 'El poder de la rapidez mental.'
     },
     {
@@ -68,7 +68,7 @@ const pokeneas = [
       nombre: 'Pokenea9',
       altura: 1.1,
       habilidad: 'Camuflaje',
-      imagen: 'https://storage.googleapis.com/tu-bucket/pokenea9.jpg',
+      imagen: 'https://storage.googleapis.com/pokeneas-taller02/download%20(1).jpg',
       fraseFilosofica: 'A veces, lo que no se ve es lo más poderoso.'
     },
     {
@@ -76,9 +76,9 @@ const pokeneas = [
       nombre: 'Pokenea10',
       altura: 1.55,
       habilidad: 'Metamorfosis',
-      imagen: 'https://storage.googleapis.com/tu-bucket/pokenea10.jpg',
+      imagen: 'https://storage.googleapis.com/pokeneas-taller02/download%20(2).jpg',
       fraseFilosofica: 'La transformación es el camino hacia el éxito.'
     }
   ];
   
-  module.exports = pokeneas;  
+  module.exports = pokeneas;
