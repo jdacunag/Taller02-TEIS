3fe52bebea82a968f785d25e14005ef614324a99
Verónica Zapata Vargas <112635115+solobero@users.noreply.github.com>
2024-11-12 19:34:56 -0500

    Update pokeneasData.js phrases

--- src/data/pokeneasData.js
+++ src/data/pokeneasData.js
@@ -1,84 +1,84 @@
 const pokeneas = [
     {
       id: 1,
-      nombre: 'Pokenea1',
-      altura: 1.5,
-      habilidad: 'Invisibilidad',
-      imagen: 'https://storage.googleapis.com/pokeneas-taller02/(%E2%88%A9%5Eo%5E)%E2%8A%83%E2%94%81%E2%98%86.jpg',
-      fraseFilosofica: 'El conocimiento es poder.'
+      name: 'Pokenea1',
+      height: 1.5,
+      ability: 'Invisibilidad',
+      image: 'https://storage.googleapis.com/pokeneas-taller02/(%E2%88%A9%5Eo%5E)%E2%8A%83%E2%94%81%E2%98%86.jpg',
+      philosophicalQuote: 'La vida es un visaje.'
     },
     {
       id: 2,
-      nombre: 'Pokenea2',
-      altura: 1.2,
-      habilidad: 'Velocidad',
-      imagen: 'https://storage.googleapis.com/pokeneas-taller02/288149-origen-todos-nombres-pokemon-i.webp',
-      fraseFilosofica: 'La paciencia es una virtud.'
+      name: 'Pokenea2',
+      height: 1.2,
+      ability: 'Velocidad',
+      image: 'https://storage.googleapis.com/pokeneas-taller02/288149-origen-todos-nombres-pokemon-i.webp',
+      philosophicalQuote: 'Solo Dios, familia y Atletico Nacional.'
     },
     {
       id: 3,
-      nombre: 'Pokenea3',
-      altura: 1.8,
-      habilidad: 'Fuerza',
-      imagen: 'https://storage.googleapis.com/pokeneas-taller02/004.png',
-      fraseFilosofica: 'La fuerza reside en el corazón.'
+      name: 'Pokenea3',
+      height: 1.8,
+      ability: 'Fuerza',
+      image: 'https://storage.googleapis.com/pokeneas-taller02/004.png',
+      philosophicalQuote: 'Como me dejo meter este ganso ciego hoy 24 ome.'
     },
     {
       id: 4,
-      nombre: 'Pokenea4',
-      altura: 1.3,
-      habilidad: 'Regeneración',
-      imagen: 'https://storage.googleapis.com/pokeneas-taller02/Cubone.jpg',
-      fraseFilosofica: 'El cambio es la única constante.'
+      name: 'Pokenea4',
+      height: 1.3,
+      ability: 'Regeneración',
+      image: 'https://storage.googleapis.com/pokeneas-taller02/Cubone.jpg',
+      philosophicalQuote: 'La buena mi fafai.'
     },
     {
       id: 5,
-      nombre: 'Pokenea5',
-      altura: 1.6,
-      habilidad: 'Telepatía',
-      imagen: 'https://storage.googleapis.com/pokeneas-taller02/Estos%20son%20los%20100%20Pok%C3%A9mon%20m%C3%A1s%20cuquis%20y%20su%20ataque%20especial%20se%20llama_%20mu%C3%A9rete%20de%20amor.jpg',
-      fraseFilosofica: 'Todo comienza en la mente.'
+      name: 'Pokenea5',
+      height: 1.6,
+      ability: 'Telepatía',
+      image: 'https://storage.googleapis.com/pokeneas-taller02/Estos%20son%20los%20100%20Pok%C3%A9mon%20m%C3%A1s%20cuquis%20y%20su%20ataque%20especial%20se%20llama_%20mu%C3%A9rete%20de%20amor.jpg',
+      philosophicalQuote: 'Todo bien.'
     },
     {
       id: 6,
-      nombre: 'Pokenea6',
-      altura: 1.7,
-      habilidad: 'Control del tiempo',
-      imagen: 'https://storage.googleapis.com/pokeneas-taller02/Pokem%C3%B3n%20PNG%20descarga%20gratis.jpg',
-      fraseFilosofica: 'El tiempo es relativo.'
+      name: 'Pokenea6',
+      height: 1.7,
+      ability: 'Control del tiempo',
+      image: 'https://storage.googleapis.com/pokeneas-taller02/Pokem%C3%B3n%20PNG%20descarga%20gratis.jpg',
+      philosophicalQuote: 'Eso es bala o polvora.'
     },
     {
       id: 7,
-      nombre: 'Pokenea7',
-      altura: 1.4,
-      habilidad: 'Resistencia extrema',
-      imagen: 'https://storage.googleapis.com/pokeneas-taller02/VULPIX.jpg',
-      fraseFilosofica: 'La persistencia rompe todas las barreras.'
+      name: 'Pokenea7',
+      height: 1.4,
+      ability: 'Resistencia extrema',
+      image: 'https://storage.googleapis.com/pokeneas-taller02/VULPIX.jpg',
+      philosophicalQuote: 'Szs.'
     },
     {
       id: 8,
-      nombre: 'Pokenea8',
-      altura: 1.9,
-      habilidad: 'Reflejos',
-      imagen: 'https://storage.googleapis.com/pokeneas-taller02/c157545ab985be295c9566b427102231.jpg',
-      fraseFilosofica: 'El poder de la rapidez mental.'
+      name: 'Pokenea8',
+      height: 1.9,
+      ability: 'Reflejos',
+      image: 'https://storage.googleapis.com/pokeneas-taller02/c157545ab985be295c9566b427102231.jpg',
+      philosophicalQuote: 'Pa que zapatos si no hay plata.'
     },
     {
       id: 9,
-      nombre: 'Pokenea9',
-      altura: 1.1,
-      habilidad: 'Camuflaje',
-      imagen: 'https://storage.googleapis.com/pokeneas-taller02/download%20(1).jpg',
-      fraseFilosofica: 'A veces, lo que no se ve es lo más poderoso.'
+      name: 'Pokenea9',
+      height: 1.1,
+      ability: 'Camuflaje',
+      image: 'https://storage.googleapis.com/pokeneas-taller02/download%20(1).jpg',
+      philosophicalQuote: 'Oe parcero me dice la hora.'
     },
     {
       id: 10,
-      nombre: 'Pokenea10',
-      altura: 1.55,
-      habilidad: 'Metamorfosis',
-      imagen: 'https://storage.googleapis.com/pokeneas-taller02/download%20(2).jpg',
-      fraseFilosofica: 'La transformación es el camino hacia el éxito.'
+      name: 'Pokenea10',
+      height: 1.55,
+      ability: 'Metamorfosis',
+      image: 'https://storage.googleapis.com/pokeneas-taller02/download%20(2).jpg',
+      philosophicalQuote: 'Uno a veces cree que tin pero tan.'
     }
   ];
   
-  module.exports = pokeneas;  
+  module.exports = pokeneas;
